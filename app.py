2ca06c888074ca55613ddf0a0444fee0130ced70
massivebird <33991373+massivebird@users.noreply.github.com>
2025-03-22 19:04:47 -0400

    feat: CSS styling

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    f = open("index.html", "r")
+def file_contents(pathname):
+    f = open(pathname, "r")
     return f.read()
+
+@app.route("/")
+def main():
+    return file_contents("index.html")
+
+@app.route("/style.css")
+def styles():
+    f = open("style.css", "r")
+    return f.read()
+
+with app.test_request_context():
+    url_for('static', filename='style.css')
